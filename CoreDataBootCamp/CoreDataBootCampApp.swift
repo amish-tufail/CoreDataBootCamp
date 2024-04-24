5c8007b66e4b37a6fc2eb9d9373e0544f82884e7
Amish <amish.tufail2002@gmail.com>
2024-04-24 09:36:38 +0500

    Initial Commit

---
+++ CoreDataBootCamp/CoreDataBootCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataBootCampApp.swift
+//  CoreDataBootCamp
+//
+//  Created by Amish on 24/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataBootCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
